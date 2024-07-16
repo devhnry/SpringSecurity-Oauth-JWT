c3b26cb9d4d5e592572a953f13a8d6d389877878
devhnry <devwhenry@gmail.com>
2024-07-16 15:21:25 +0100

    feat: implented service to generate token and refresh token

--- Backend/src/main/java/com/dev/h3nry/entity/AppUser.java
+++ Backend/src/main/java/com/dev/h3nry/entity/AppUser.java
@@ -1,4 +1,38 @@
 package com.dev.h3nry.entity;
 
-public class AppUser {
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor @NoArgsConstructor
+@Setter @Getter @ToString
+public class AppUser implements UserDetails {
+
+    @Id
+    @Column(unique = true, nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String email;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
--- Backend/src/main/java/com/dev/h3nry/service/JwtService.java
+++ Backend/src/main/java/com/dev/h3nry/service/JwtService.java
@@ -1,4 +1,81 @@
 package com.dev.h3nry.service;
 
-public class JwtService {
+import com.dev.h3nry.entity.AppUser;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import static io.jsonwebtoken.SignatureAlgorithm.HS256;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.function.Function;
+
+@Service
+@Slf4j
+public class JwtService{
+
+    private final SecretKey secretKey;
+    private final byte[] secretKeyBytes;
+
+    public JwtService(@Value("${secretString}") String secretString) {
+        secretKeyBytes = Base64.getDecoder().decode(secretString.getBytes(StandardCharsets.UTF_8));
+        this.secretKey = new SecretKeySpec(secretKeyBytes, String.valueOf(HS256));
+    }
+
+    public String createToken(AppUser user) {
+        log.info("Generating JWT token");
+        return generateToken(user);
+    }
+
+    private String generateToken(AppUser user) {
+        return Jwts.builder()
+                .claim("userID", user.getId())
+                .claim("username", user.getUsername())
+                .setSubject(user.getUsername())
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(Instant.now().plus(24, ChronoUnit.HOURS)))
+                .signWith(HS256, secretKey)
+                .compact();
+    }
+
+    public String generateRefreshToken(HashMap<String, Object> claims, AppUser user){
+        return Jwts.builder()
+                .claim("userId", user.getId())
+                .setClaims(claims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(Instant.now().plus(10, ChronoUnit.HOURS)))
+                .signWith(HS256, secretKey)
+                .compact();
+    }
+
+    public String extractUsername(String token){
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaims(token, Claims::getExpiration).before(new Date());
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public <T> T extractClaims(String token, Function<Claims, T> claimsTFunction){
+        return claimsTFunction.apply(Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody());
+    }
+
 }
