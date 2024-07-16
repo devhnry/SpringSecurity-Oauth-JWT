cd8a9d42534cdff1825db15c86c5710b8148f69a
devhnry <devwhenry@gmail.com>
2024-07-16 14:42:49 +0100

    build: project boostrapping

---
+++ Backend/src/test/java/com/dev/h3nry/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.h3nry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
