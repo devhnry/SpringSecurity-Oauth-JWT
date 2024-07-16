2eed8648a548b0ed2b7ed3bfc95a224ada9e89a8
devhnry <devwhenry@gmail.com>
2024-07-16 15:46:33 +0100

    feat: setup security config to protect endpoints

--- Backend/src/main/java/com/dev/h3nry/config/SecurityConfig.java
+++ Backend/src/main/java/com/dev/h3nry/config/SecurityConfig.java
@@ -1,7 +1,36 @@
 package com.dev.h3nry.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtSecurityFilter jwtSecurityFilter;
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(request -> request
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/api/v1/**").authenticated()
+                ).sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider.authenticationProvider())
+                .addFilterBefore(jwtSecurityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
