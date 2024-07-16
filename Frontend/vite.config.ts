1897e4c06265de97ee64f19b438acd6b745b3e92
devhnry <devwhenry@gmail.com>
2024-07-16 16:06:22 +0100

    build: setup vite.config to build files to backend static package

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+//https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "../Backend/src/main/resources/static/",
+    emptyOutDir: true, // also necessary
+  },
+  resolve: {
+    alias: {
+      "@": "./src"
+    },
+  },
+});
